13916390b1de2b53f75e0fbe04bc3d41916daed3
sanghpriya1 <sanghpriya2009@gmail.com>
2021-05-22 12:47:23 +0530

    String reverse

---
+++ src/Stringss.java
@@ -0,0 +1,13 @@
+
+public class Stringss {
+	public static void main(String[] args) {
+		String A = "madam";
+		String R = new StringBuilder(A).reverse().toString();
+		if(A.equals(R))System.out.println("Yes");
+		else System.out.println("No");
+		
+		
+		
+	}
+
+}
